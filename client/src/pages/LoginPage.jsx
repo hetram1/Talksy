f73b41e8b62c057588f30690ec8f50c33ed8e766
hetram1 <hetramgugrwal13@gmail.com>
2025-05-16 15:30:04 +0530

    Home page and side bar created

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>Login Page</div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>Profile Page</div>
+  )
+}
+
+export default ProfilePage
